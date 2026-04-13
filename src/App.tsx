032eac7485654f02e2234b38106da5534aab4d34
Paulo Antonio <paulo.werder@gmail.com>
2026-04-13 10:54:35 -0300

    tipagem typescript children reactnode

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { Heading } from './components/Heading';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <div>
-      <Heading attrs={123} atr='String'>
-        Olá Mundo!
+      <Heading>
+        Olá Mundo!{' '}
+        <button>
+          <TimerIcon />
+        </button>
       </Heading>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident velit
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,5 +1,14 @@
 import styles from './Heading.module.css';
 
-export function Heading(props) {
-  return <h1 className={styles.cyan}>{props.children}</h1>;
+// Definindo um tipo para a props
+type HeadingProps = {
+  // tudo que o react aceita como children
+  children: React.ReactNode;
+};
+
+/**
+ * children é um destruração do javascript, ele vem do props.children
+ */
+export function Heading({ children }: HeadingProps) {
+  return <h1 className={styles.heading}>{children}</h1>;
 }
