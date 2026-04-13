63dc85f90feca53ab5bd5fe136f710bc05fa3778
Paulo Antonio <paulo.werder@gmail.com>
2026-04-13 11:26:36 -0300

    criando o proprio componente do container

---
+++ src/components/Container.tsx
@@ -0,0 +1,14 @@
+import styles from './Container.module.css';
+import type { ReactNode } from 'react';
+
+type ContainerProps = {
+  children: ReactNode;
+};
+
+export function Container({ children }: ContainerProps) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>{children}</div>
+    </div>
+  );
+}
